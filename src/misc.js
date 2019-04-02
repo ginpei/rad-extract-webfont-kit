7d7fbc8eb461b7be4d12215b1c5c073382443c25
ginpei <ginpei@gmail.com>
2019-04-01 17:42:39 -0700

    unnecessary async

--- src/misc.js
+++ src/misc.js
@@ -129,7 +129,7 @@ module.exports.extractFontKit = extractFontKit;
  * @param {string} filePath
  * @returns {Promise<string>}
  */
-async function readText (filePath) {
+function readText (filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, text) => {
       if (err) {
