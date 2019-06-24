724f2e4f92559b32ca27b299af3c3af97650aacd
ginpei <ginpei@gmail.com>
2019-06-24 01:01:42 -0700

    simplify

--- src/generateMeta/index.js
+++ src/generateMeta/index.js
@@ -18,61 +18,26 @@ const {
   isLinotype,
 } = require('./linotype');
 
-/** @enum {string} */
-const KitType = {
-  fontsCom: 'fontsCom',
-  fontsComMulti: 'fontsComMulti',
-  fontSquirrelCom: 'fontSquirrelCom',
-  linotype: 'linotype',
-  unknown: 'unknown',
-};
-
 /**
- * Detect which type of webfont kit is the target.
+ * Generate meta data JSON file to use web fonts.
  * @param {string} dir
- * @returns {Promise<KitType>}
+ * @returns {Promise<IFontMeta>}
  */
-async function detectKitType (dir) {
+module.exports = async (dir) => {
   if (await isFontsComMulti(dir)) {
-    return KitType.fontsComMulti;
+    return createFontsComMultiMeta(dir);
   }
 
   if (await isFontsCom(dir)) {
-    return KitType.fontsCom;
+    return createFontsComMeta(dir);
   }
 
   if (await isFontSquirrel(dir)) {
-    return KitType.fontSquirrelCom;
+    return createFontSquirrelMeta(dir);
   }
 
   if (await isLinotype(dir)) {
-    return KitType.linotype;
-  }
-
-  return KitType.unknown;
-}
-
-/**
- * Generate meta data JSON file to use web fonts.
- * @param {string} srcDir
- * @returns {Promise<IFontMeta>}
- */
-module.exports = async (srcDir) => {
-  const type = await detectKitType(srcDir);
-  if (type === KitType.fontsComMulti) {
-    return createFontsComMultiMeta(srcDir);
-  }
-
-  if (type === KitType.fontsCom) {
-    return createFontsComMeta(srcDir);
-  }
-
-  if (type === KitType.fontSquirrelCom) {
-    return createFontSquirrelMeta(srcDir);
-  }
-
-  if (type === KitType.linotype) {
-    return createLinotypeMeta(srcDir);
+    return createLinotypeMeta(dir);
   }
 
   throw new Error('Unsupported type of webfont kit');
